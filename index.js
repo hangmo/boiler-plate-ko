d4e0792195b53bb38a0fbcbe5200da47c97eb3e4
DESKTOP-H1U9K25\NB-0410 <hangmo.cho@dkitec.com>
2021-06-09 11:39:08 +0900

    비보

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require('./models/User');
+
+// Application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Application/JSON
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://root:1234@boilerplate.hpbz9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+
+console.log('config.mongoURI : ', config.mongoURI , config);
+
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -14,8 +26,20 @@ mongoose.connect('mongodb+srv://root:1234@boilerplate.hpbz9.mongodb.net/myFirstD
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+res.send('Hello World! ~~~~~')
 })
+
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if(err) return res.json({success:false, err});
+    return res.status(200).json({
+      success: true
+    })
+  })
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
